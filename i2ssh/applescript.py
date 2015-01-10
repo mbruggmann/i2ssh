d0d85fb23a615f8a9bfacbe7efb301a2f1e32c2d
Marc Bruggmann <bruggmann.marc@gmail.com>
2015-01-10 18:19:21 +0100

    Split the layout before executing the command.

--- i2ssh/applescript.py
+++ i2ssh/applescript.py
@@ -10,40 +10,60 @@ import tempfile
 # Author: Luis Martin Gil
 # Website: http://www.luismartingil.com/
 
-pane_template = '    set tab1 to tab1 & {{cmd:"%s", name:"%s", split:"%s"}}'
+pane_template = '    set panes to panes & {{cmd:"%s", name:"%s"}}'
+
+layout_template = '''
+        -- layout %s
+        set layout to {"%s"}
+        repeat with currentLayout in items of layout
+            tell i term application "System Events" to keystroke currentLayout using command down
+        end repeat
+'''
 
 code_template = '''
 tell application "iTerm"
-    set tab1 to {}
+    set panes to {}
 %s
     set myterm to (make new terminal)
     tell myterm
         launch session 1
-        repeat with currentPane in items of tab1
-            tell the last session
-                -- set name to name of currentPane
-                write text cmd of currentPane
-                if "h" is split of currentPane then
-                    tell i term application "System Events" to keystroke "D" using command down
-                else if "v" is split of currentPane then
-                    tell i term application "System Events" to keystroke "d" using command down
-                end if
-            end tell
+        %s
+        repeat with currentPane in items of panes
             delay 1
+            tell the current session
+                set name to name of currentPane
+                write text cmd of currentPane
+                tell i term application "System Events" to keystroke "]" using command down
+            end tell
         end repeat
     end tell
 end tell
 '''
 
+def pane_snippet(cmd=None, name="Default"):
+    return pane_template % (cmd, name)
 
-def pane_snippet(cmd=None, name="Default", split=None):
-    return pane_template % (cmd, name, split)
+def layout_snippet(count, spec=None):
+    cols = int(spec.split('x')[0] if spec else (count+1)/2)
+    rows = int(spec.split('x')[1] if spec else count/2)
+    realspec = '%sx%s' % (cols, rows)
 
+    splits = []
+    for i in range(0, cols):
+        if i < cols-1:
+            splits.append('d')
+            splits.append('[')
+        for j in range(0, rows-1):
+            splits.append('D')
+        splits.append(']')
+
+    return layout_template % (realspec, '","'.join(splits))
 
 def launch(config):
     # Construct the applescript
-    panes = [pane_snippet('ls', hostname, 'v') for hostname in config['hosts']]
-    body = code_template % ('\n'.join(panes))
+    panes = [pane_snippet('ls', hostname) for hostname in config['hosts']]
+    layout = layout_snippet(len(config['hosts']), config['layout'])
+    body = code_template % ('\n'.join(panes), layout)
 
     # Write it to a temporary file
     temp = tempfile.NamedTemporaryFile(delete=False)
